a81926a64d826e69ec7a887c9604841c8407d660
energyshifter8 <temuulenbold1@gmail.com>
2026-02-16 13:20:12 +0800

    hello

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="hero">
+      <div className="overlay">
+        <div className="content">
+          <h1 className="main-title">
+            THE <br /> RENAISSANCE <br /> EDITION
+          </h1>
+          <p className="description">
+            A new world of commerce. 150+ product updates.
+          </p>
+
+          {/* Дата авах хэсэг (Firebase-тэй дараа холбоно) */}
+          <div className="input-group">
+            <input
+              type="text"
+              placeholder="Raw data оруулна уу..."
+              className="custom-input"
+            />
+            <button className="send-button">SEND TO FIREBASE</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
